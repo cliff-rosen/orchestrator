069a7ec433e61562bef5fa6d3e1c2de89500c6b3
Cliff Rosen <cliff.rosen@gmail.com>
2025-02-22 15:25:16 -0700

    backup

--- frontend/src/pages/Workflow.tsx
+++ frontend/src/pages/Workflow.tsx
@@ -628,8 +628,8 @@ const Workflow: React.FC = () => {
 
                 {/* Main Content */}
                 <div className="flex-1 overflow-y-auto">
-                    <div className="container mx-auto px-4">
-                        <div className="flex-1">
+                    <div className="container mx-auto px-4 py-6">
+                        <div className="flex-1 space-y-6">
                             {showConfig ? (
                                 <WorkflowConfig />
                             ) : (
